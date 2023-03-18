3e91963c49cd3075b6586e36bb2a0dc57f3538a4
AlinaF1984 <afassahova9@gmail.com>
2023-03-18 15:14:53 +0500

    add code

--- 7.3/playwright/tests/Testing.js
+++ 7.3/playwright/tests/Testing.js
@@ -1,41 +1,42 @@
 const { test, expect } = require("@playwright/test");
-const user = require('../user.js');
+const user = require("../user.js");
 
 test("Успешная авторизация", async ({ page }) => {
-    // Go to https://netology.ru/?modal=sign_in
-    await page.goto("https://netology.ru/?modal=sign_in");
+  // Go to https://netology.ru/?modal=sign_in
+  await page.goto("https://netology.ru/?modal=sign_in");
   // Click [placeholder="Email"]
-    await page.locator('[placeholder="Email"]').click();
+  await page.locator('[placeholder="Email"]').click();
   // Fill [placeholder="Email"]
-    await page.locator('[placeholder="Email"]').fill(user.email);
+  await page.locator('[placeholder="Email"]').fill(user.email);
   // Click [placeholder="Пароль"]
-    await page.locator('[placeholder="Пароль"]').click();
+  await page.locator('[placeholder="Пароль"]').click();
   // Fill [placeholder="Пароль"]
-    await page.locator('[placeholder="Пароль"]').fill(user.password);
+  await page.locator('[placeholder="Пароль"]').fill(user.password);
   // Click [data-testid="login-submit-btn"]
-    await page.locator('[data-testid="login-submit-btn"]').click();
-    await page.screenshot({ path: "screenshot.png" });
-  
-    await expect(page).toHaveURL('https://netology.ru/profile');
-    
-    await expect(page.locator('h2')).toHaveText('Мои курсы и профессии');
-  
-  });
-  
-  test("Неуспешная авторизация", async ({ page }) => {
-    // Go to https://netology.ru/?modal=sign_in
-    await page.goto("https://netology.ru/?modal=sign_in");
+  await page.locator('[data-testid="login-submit-btn"]').click();
+  await page.screenshot({ path: "screenshot.png" });
+
+  await expect(page).toHaveURL("https://netology.ru/profile");
+
+  await expect(page.locator("h2")).toHaveText("Мои курсы и профессии");
+});
+
+test("Неуспешная авторизация", async ({ page }) => {
+  // Go to https://netology.ru/?modal=sign_in
+  await page.goto("https://netology.ru/?modal=sign_in");
   // Click [placeholder="Email"]
-    await page.locator('[placeholder="Email"]').click();
+  await page.locator('[placeholder="Email"]').click();
   // Fill [placeholder="Email"]
-    await page.locator('[placeholder="Email"]').fill(user.wrongEmail);
+  await page.locator('[placeholder="Email"]').fill(user.wrongEmail);
   // Click [placeholder="Пароль"]
-    await page.locator('[placeholder="Пароль"]').click();
+  await page.locator('[placeholder="Пароль"]').click();
   // Fill [placeholder="Пароль"]
-    await page.locator('[placeholder="Пароль"]').fill(user.wrongPassword);
+  await page.locator('[placeholder="Пароль"]').fill(user.wrongPassword);
   // Click [data-testid="login-submit-btn"]
-    await page.locator('[data-testid="login-submit-btn"]').click();
-    
-    await expect(page.locator('[data-testid="login-error-hint"]')).toHaveText('Вы ввели неправильно логин или пароль');
-    await page.screenshot({ path: "screenshotError.png" });
-  });
+  await page.locator('[data-testid="login-submit-btn"]').click();
+
+  await expect(page.locator('[data-testid="login-error-hint"]')).toHaveText(
+    "Вы ввели неправильно логин или пароль"
+  );
+  await page.screenshot({ path: "screenshotError.png" });
+});
